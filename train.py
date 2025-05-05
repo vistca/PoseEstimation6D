29ee047b4d510e952d57da8262c167c85e7fb292
vistca <93658028+vistca@users.noreply.github.com>
2025-05-05 14:45:55 +0200

    updates and refactoring of train and test functions

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from test import test
+import torch
 import tqdm
 
 class Trainer():
@@ -7,14 +9,14 @@ class Trainer():
         self.optimizer = optimizer
         self.wandb_instance = wandb_instance
 
-    def train(self, dataloader, loss_fn):
+    def train(self, train_dataloader, val_dataloader, loss_fn):
         self.model.train()
         running_loss = 0.0
         correct = 0
         total = 0
 
         nr_batches = 0
-        for (inputs, targets) in tqdm(dataloader):
+        for (inputs, targets) in tqdm(train_dataloader):
             inputs, targets = inputs.cuda(), targets.cuda()
 
             pred = self.model(inputs)
@@ -31,12 +33,19 @@ class Trainer():
 
             nr_batches += 1
 
-
-
+        
         train_loss = running_loss / nr_batches #len(dataloader)
         train_accuracy = 100. * correct / total
+        
         print(f'Loss: {train_loss:.6f} Acc: {train_accuracy:.2f}%')
 
-        self.wandb_instance.log_metric({"training_loss" : train_loss, "training_accuracy" : train_accuracy})
+        val_accuracy, val_loss = test(self.model, val_dataloader, loss_fn)
 
+        print(f'Validation Loss: {val_loss:.6f} Acc: {val_accuracy:.2f}%')
 
+        self.wandb_instance.log_metric({"training_loss" : train_loss, 
+                                        "training_accuracy" : train_accuracy,
+                                        "validation_loss" : val_loss,
+                                        "validation_accuracy" : val_accuracy})
+
+    
