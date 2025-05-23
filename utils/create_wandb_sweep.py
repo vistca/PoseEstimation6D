bee8ba1432d6b21ed026e175a10e779f411fd949
Felix Erngård <felix@Mac.fritz.box>
2025-05-23 16:26:19 +0200

    cahnges to sweep creation

--- utils/create_wandb_sweep.py
+++ utils/create_wandb_sweep.py
@@ -2,24 +2,44 @@ import wandb
 import yaml
 import argparse
 import os
+import wandb
 
 if __name__ == "__main__":
-
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--project', type=str, help='Id of the sweep', default="PoseEstimation6D")
-        parser.add_argument('--wb', type=str, help='wb key', default="")
-        args = parser.parse_args()
-
-        with open('config/sweep_config.yaml') as f:
-                sweep_conf = yaml.safe_load(f)
-
-        sweep_id = wandb.sweep(sweep_conf, 
-                        entity="fantastic_4_0",
-                        project=args.project)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--project', type=str, help='project in which the sweep is to be located', default="PoseEstimation6D")
+    parser.add_argument('--wb', type=str, help='wb key', default="")
+    parser.add_argument('--sf', type=str, help='file location of sweep', default="config/phase2_sweep_config.yaml")
+    args = parser.parse_args()
+    
+    
+    if args.wb == "":
+        try:
+           file = open("wandb_api_key.txt").readlines()
+           for lines in file:
+               args.wb = lines
+        except:
+           raise("Login to wandb failed, a key was not provided")
         
-        if not os.path.exists("./wandb_api_key.txt") and args.wb != "":
-                with open("./wandb_api_key.txt", "w") as file:
-                        file.write(args.wb)
+    else:
+        try:
+           wandb.login(key=args.wb)
+        except:
+           raise("Login to wandb failed, provided key is invalid")
+    
+
+    try:
+        with open(args.sf) as f:
+           sweep_conf = yaml.safe_load(f)
+    except:
+        raise("File path is unvalid")
+
+    sweep_id = wandb.sweep(sweep_conf, 
+                           entity="fantastic_4_0",
+                           project=args.project)
+
+    if not os.path.exists("./wandb_api_key.txt") and args.wb != "":
+        with open("./wandb_api_key.txt", "w") as file:
+           file.write(args.wb)
 
 
-        print(sweep_id)
+    print(sweep_id)
