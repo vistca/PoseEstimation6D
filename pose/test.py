b037a06ea2465b2eb81dd3f7cead326ea74ca5a1
unknown <sidenerik@gmail.com>
2025-05-27 16:53:16 +0200

    implemented ADD

--- pose/test.py
+++ pose/test.py
@@ -3,6 +3,8 @@ import time
 import statistics
 import torch
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
+import numpy as np
+from plyfile import PlyData
 
 class Tester():
 
@@ -11,6 +13,21 @@ class Tester():
         self.wandb_instance = wandb_instance
         self.loss_fn = torch.nn.MSELoss()
 
+    def compute_ADD(self, model_points, R_gt, t_gt, R_pred, t_pred):
+        pts_gt = (R_gt @ model_points.T).T + t_gt.reshape(1, 3)
+        pts_pred = (R_pred @ model_points.T).T + t_pred.reshape(1, 3)
+        return np.mean(np.linalg.norm(pts_gt - pts_pred, axis=1))
+    
+    def load_model_points(self, ply_path):
+        plydata = PlyData.read(ply_path)
+        vertex_data = plydata['vertex']
+        model_points = np.stack([vertex_data['x'], vertex_data['y'], vertex_data['z']], axis=-1)
+        return model_points
+    
+    def get_ply_file(self, obj_id):
+        if (obj_id < 10):
+            return "obj_0" + str(obj_id) + ".ply"
+        return "obj_" + str(obj_id) + ".ply"
 
     def validate(self, dataloader, device, type):
 
@@ -48,16 +65,30 @@ class Tester():
 
                 # doing it like this takes forever, might need to check this and update it accordingly
 
-                self.model.train()
                 preds = self.model(inputs)
-                self.model.eval()
 
                 #print(type(loss_dict), loss_dict)  # Debugging output
                 loss = self.loss_fn(preds, targets)
 
+                # Calculate the ADD metric
+                models_folder = "./datasets/Linemod_preprocessed/models/"
+                for i in range(nr_datapoints):
+                    pred = preds[i]
+                    gt = targets[i]
+                    t_pred = pred[:3].reshape(3,1)
+                    R_pred = pred[3:].reshape(3,3)
+                    t_gt = gt[:3].reshape(3,1)
+                    R_gt = gt[3:].reshape(3,3)
+                    obj_id = int(batch["obj_id"][i].item())
+                    ply_file = self.get_ply_file(obj_id)
+                    model_points = self.load_model_points(models_folder + ply_file)
+                    add = self.compute_ADD(model_points, R_gt, t_gt, R_pred, t_pred)
+                    print("The add is: " + str(add))
                 val_loss += loss
 
                 progress_bar.set_postfix(total=val_loss/(batch_id + 1))
+
+                break
 
         avg_loss = val_loss / len(dataloader)
 
