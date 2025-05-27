53a082e19afc7d09cf20bc0334a916b644ded9ab
vistca <93658028+vistca@users.noreply.github.com>
2025-05-27 20:55:27 +0200

    removed breaks

--- pose/train.py
+++ pose/train.py
@@ -2,7 +2,7 @@ from tqdm import tqdm
 import time
 import statistics
 import torch
-from torch.cuda.amp import autocast, GradScaler
+from torch.amp import autocast, GradScaler
 
 class Trainer():
 
@@ -96,8 +96,6 @@ class Trainer():
             nr_batches += 1
 
             progress_bar.set_postfix(total=total_loss/nr_batches)
-
-            break
 
 
         avg_loss = total_loss / len(train_loader)
