f2019fcd561dcccf2f12b8bac2870d4330159576
Vilho <vilho.lamsa@aalto.fi>
2025-05-29 14:59:44 +0200

    Training data augmentation and normalization.

--- data/faster_dataset.py
+++ data/faster_dataset.py
@@ -56,10 +56,18 @@ class FasterDataset(Dataset):
         # Define image transformations
         self.train_transform = transforms.Compose([
             transforms.ToTensor(),
+
+            transforms.ColorJitter(),
+            transforms.GaussianBlur(3),
+
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
         ])
 
         self.val_test_transform = transforms.Compose([
             transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
         ])
 
         self.load_sample_confs()
