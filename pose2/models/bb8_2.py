d98d39e3c8a8d107dda65590717fa4d3fa014ddb
vistca <93658028+vistca@users.noreply.github.com>
2025-06-05 22:19:26 +0200

    error fixed out to in for fc in model

--- pose2/models/bb8_2.py
+++ pose2/models/bb8_2.py
@@ -6,7 +6,7 @@ class BB8Model_2(nn.Module):
         super().__init__()
         # We use ResNet18 as the backbone instead of VGG for greater efficiency
         self.backbone = resnet50(weights="DEFAULT")
-        in_channels = self.backbone.fc.out_features
+        in_channels = self.backbone.fc.in_features
         self.backbone.fc = nn.Identity()  # We remove the final fully connected layer
 
         # Head for predicting 2D points (8 corners * 2 coordinates)
